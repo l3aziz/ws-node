088e2b4dfd602ab1078b66cd796f71324d5291d7
l3aziz <gtariaziz4@gmail.com>
2023-01-12 19:05:14 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.write("<h1>Hello Node!!!!</h1>");
+    res.end();
+  }
+});
+
+server.listen(3000, (err) => {
+  if (err) throw err;
+  console.log("server is up and running...");
+});
